b12f355c3023652a6a756cf365eb791b2657e5d4
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-21 19:06:41 +0300

    added title component with props

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <script>
 	import Navbar from './Navbar.svelte';
+	import Title from './Title.svelte';
 </script>
 
 <Navbar />
+<Title title="add expense" />
+<Title title="expense list" />
+<Title />
