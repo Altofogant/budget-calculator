e8df1017e1d7c63dac6e7bd4229a5d10d4fa8fa1
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-22 15:02:27 +0300

    added expense delete functionality

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,13 @@
 	import ExpensesList from './ExpensesList.svelte';
 
 	let expenses = [...ExpensesData];
+
+	function removeExpense(id) {
+		expenses = expenses.filter(item => item.id !== id);
+	}
 </script>
 
 <Navbar />
 <main class="content">
-	<ExpensesList expenses={expenses} />
+	<ExpensesList {expenses} {removeExpense}/>
 </main>
