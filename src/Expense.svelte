b0c96a37bc5d4fd84ff96a6d337d423d455e3d12
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-22 14:17:13 +0300

    passing expense props to expense component

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -1,1 +1,10 @@
-<h1>This is a single expense</h1>
+<script>
+    export let id;
+    export let name = '';
+    export let amount = 0;
+</script>
+
+<div>
+    <h1>Title: {name}</h1>
+    <h1>amount: {amount}</h1>
+</div>
