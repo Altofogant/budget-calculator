e186eea591fc6326ffb089ddb08daddf9deaae0e
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-23 21:09:21 +0300

    set/get context for expense removal

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,15 @@
 	import Navbar from './Navbar.svelte';
 	import ExpensesData from './expenses.js';
 	import ExpensesList from './ExpensesList.svelte';
+	import { setContext } from 'svelte';
 
 	let expenses = [...ExpensesData];
 
 	function removeExpense(id) {
 		expenses = expenses.filter(item => item.id !== id);
 	}
+
+	setContext('remove', removeExpense);
 </script>
 
 <Navbar />
