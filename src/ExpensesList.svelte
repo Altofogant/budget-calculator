ba46b225f3fd8ecc7dd79c8c2ca1da0b22ac4bec
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-29 14:31:09 +0300

    added transitions and animations to the expense list items

--- src/ExpensesList.svelte
+++ src/ExpensesList.svelte
@@ -1,6 +1,8 @@
 <script>
     import SectionTitle from './Title.svelte';
     import Expense from './Expense.svelte';
+    import { fly } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
     export let expenses = [];
     export let removeExpense;
 </script>
@@ -14,8 +16,10 @@
 <section>
     <SectionTitle title="expense list" />
     <ul>
-        {#each expenses as expense, index}
+        {#each expenses as expense, index (expense.id)}
+        <div transition:fly={{ x:200, delay:index*50 }} animate:flip="{{duration: 200}}">
             <Expense {...expense} />
+        </div>
         {:else}
             <h2>No expenses added to the list</h2>
         {/each}
